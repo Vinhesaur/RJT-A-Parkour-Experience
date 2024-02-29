b9e00f3ad048ea6dd112c0ac8b45937ff31224bb
Vinhesaur <113731380+Vinhesaur@users.noreply.github.com>
2024-02-29 16:01:48 -0500

    UI Recovery

--- RJT APE/Assets/Vinh/Scripts/UI/ChangeScene.cs
+++ RJT APE/Assets/Vinh/Scripts/UI/ChangeScene.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-   public void MoveToScene (int sceneID)
+    public string sceneName;
+   public void MoveToScene ()
     {
-        SceneManager.LoadScene(sceneID);
+        SceneManager.LoadScene(sceneName);
     }
 }
