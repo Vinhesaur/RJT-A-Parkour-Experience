d330b3247228305a8c3f31d64803ac71cfaca02c
Ngxkd <danieln7@msu.edu>
2024-03-02 22:57:39 -0500

    Updating Animations/Controller

---
+++ RJT APE/Assets/Nick/Animations/Animations.cs
@@ -0,0 +1,14 @@
+//Animations
+//  Idle: 0-182
+//  Run: 233-285
+//  Sprint: 323-368
+//  Jump: 410-518
+//  Falling: 558-708
+//  Frontflip: 748-796
+//  Backflip: 836-884
+//  Frontspin: 924-972
+//  Backspin: 1012-1060
+//  Holy Flip: 1100-1148
+//  Clockwise: 1188-1238
+//  Counter Clockwise: 1278-1328
+//  Climbing: 1368-1433
