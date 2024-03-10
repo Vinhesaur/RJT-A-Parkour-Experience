eb3836595a28442769f56cf1e97a8e782bfb00e8
Ngxkd <danieln7@msu.edu>
2024-03-10 16:58:35 -0400

    Youtube Parkour game scene push

---
+++ RJT APE/Assets/Yotutube Playlist Files/ParkourControllerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkourControllerScript : MonoBehaviour
+{
+    public EnvironmentChecker environmentChecker;
+
+    void Update()
+    {
+        var hitData = environmentChecker.CheckObstacle();
+
+       if(hitData.hitFound)
+        {
+            Debug.Log("Object Founded" + hitData.hitInfo.transform.name);
+        }
+    }
+}
---
+++ RJT APE/Assets/Yotutube Playlist Files/PlayerScript.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float movementSpeed = 5f;
+    public CameraController CC;
+    public float rotSpeed = 600f;
+    Quaternion requiredRotation;
+    public Animator animator;
+
+    [Header("Player Collision & Gravity")]
+    public CharacterController cC;
+    public float surfaceCheckRadius = 0.3f;
+    public Vector3 surfaceCheckOffset;
+    public LayerMask surfaceLayer;
+    bool onSurface;
+    [SerializeField]float fallingSpeed;
+    [SerializeField] Vector3 moveDir;
+
+    private void Update()
+    {
+        if(onSurface)
+        {
+            fallingSpeed = 0f;
+        }
+        else
+        {
+            fallingSpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        var velocity = moveDir * movementSpeed;
+        velocity.y = fallingSpeed;
+
+        PlayerMovement();
+        SurfaceCheck();
+        Debug.Log("Player on Surface" + onSurface);
+    }
+
+    void PlayerMovement()
+        {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float movementAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+
+        var movementInput = (new Vector3(horizontal, 0, vertical)).normalized;
+
+        var movementDirection = CC.flatRotation * movementInput;
+
+        cC.Move(movementDirection * movementSpeed * Time.deltaTime);
+
+        if (movementAmount > 0)
+        {
+            requiredRotation = Quaternion.LookRotation(movementDirection);
+        }
+
+        movementDirection = moveDir;
+
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, requiredRotation, rotSpeed * Time.deltaTime);
+
+        animator.SetFloat("movementValue",  movementAmount, 0f, Time.deltaTime);
+
+        }
+    void SurfaceCheck()
+    {
+        onSurface = Physics.CheckSphere(transform.TransformPoint(surfaceCheckOffset), surfaceCheckRadius, surfaceLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.TransformPoint(surfaceCheckOffset), surfaceCheckRadius);
+    }
+
+}
