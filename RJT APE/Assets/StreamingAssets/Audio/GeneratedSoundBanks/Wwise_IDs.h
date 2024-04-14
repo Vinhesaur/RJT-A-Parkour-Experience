8251d0465899a651ada155f7737ebe567ec14a53
Vinhesaur <113731380+Vinhesaur@users.noreply.github.com>
2024-04-14 12:26:06 -0400

    Wwise Push

--- RJT APE/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ RJT APE/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,44 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID BREATHING = 238653005U;
+        static const AkUniqueID ENTER_ROOM = 2409810141U;
+        static const AkUniqueID FOOT = 3031504797U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LAND = 674522502U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOT
+        {
+            static const AkUniqueID GROUP = 3031504797U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace FOOT
+
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
